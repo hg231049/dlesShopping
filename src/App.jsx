62c376dde60c29b3a4003e107e524c7e355a5f86
이은서 <hg231049@gamil.com>
2026-05-15 14:43:46 +0900

    fix: vercel routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Route,Routes,useNavigate,Navigate } from 'react-router-dom';
+import { Route,Routes,useNavigate } from 'react-router-dom';
 import './App.css';
 import Header from './components/layout/header/Header';
 import Home from './components/main/Home';
